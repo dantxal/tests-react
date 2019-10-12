3463d5899a6443e9499c4179fc887a771294c4c5
Daniel <dantxal@gmail.com>
2019-10-11 22:03:00 -0300

    Adds coverage script + config react-app-rewired
    -According to PRs #240 & #241, the configs set to jest inside package.json
     are merged, and not overriden. To make it so the configs are overriden, I set the
    jest config inside the config-overrides.
    -Since last updates jest is only detecting tests inside src idk why. So moved '__tests__'
    inside src, then had to set the coverage to ignore 'src/__tests__/*'.
    -Also had to add src to 'collectCoverageFrom'.

--- config-overrides.js
+++ config-overrides.js
@@ -1,1 +1,18 @@
-module.exports = {};
+module.exports = {
+  jest: (config) => {
+    config.testMatch = ["**/__tests__/**/*.test.js"]
+    config.setupFilesAfterEnv = [
+      "@testing-library/jest-dom/extend-expect",
+      "jest-localstorage-mock"
+    ];
+    config.moduleNameMapper={
+      "^~/(.*)": "<rootDir>/src/$1"
+    };
+    config.coverageDirectory = "src/__tests__/coverage";
+    config.collectCoverageFrom = [
+      "src/**/*.js", "!src/index.js", "!src/__tests__/**/*",
+    ];
+
+    return config;
+  }
+};
